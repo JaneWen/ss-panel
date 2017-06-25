f44b65c2fcaf9a295c839cada6ac883bdbde0089
文坚 <wenjian@nancal.com>
2017-06-25 18:35:39 +0800

    管理员修改用户VIP等级后，修改数据库

--- app/Controllers/Admin/UserController.php
+++ app/Controllers/Admin/UserController.php
@@ -49,6 +49,7 @@ class UserController extends AdminController
         $user->transfer_enable = Tools::toGB($request->getParam('transfer_enable'));
         $user->invite_num = $request->getParam('invite_num');
         $user->method = $request->getParam('method');
+		$user->level = $request->getParam('level');
         $user->enable = $request->getParam('enable');
         $user->is_admin = $request->getParam('is_admin');
         $user->ref_by = $request->getParam('ref_by');
